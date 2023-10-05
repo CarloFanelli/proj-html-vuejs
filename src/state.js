bcbc5badac8ef756c30c1a4b3ae3fc866f6b5c7b
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-05 10:46:45 +0200

    edit memebers section

--- src/state.js
+++
@@ -1,9 +0,0 @@
-import { reactive } from 'vue';
-
-export const state = reactive({
-
-    menuItems: ['home', 'about us', 'portfolio', 'our process', 'pricing', 'blog', 'contact', 'get a quote'],
-
-    footerMenuItems: ['about us', 'portfolio', 'pricing', 'career', 'contact'],
-
-})
